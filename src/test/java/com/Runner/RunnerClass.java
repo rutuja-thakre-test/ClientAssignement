11f4c5b4731742b98176549052d390561fcb18d3
Rutuja Thakre <rutujathakare06@gmail.com>
2021-08-18 11:29:51 +0530

    updated

---
+++ src/test/java/com/Runner/RunnerClass.java
@@ -0,0 +1,17 @@
+package com.Runner;
+
+	import org.junit.runner.RunWith;
+	import io.cucumber.junit.Cucumber;
+	import io.cucumber.junit.CucumberOptions;
+
+	@RunWith(Cucumber.class)
+	@CucumberOptions(
+	features="./src/test/resources/features",
+	glue= "com.StepDefination",
+	plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+	)
+	public class RunnerClass {
+
+	}
+
+
